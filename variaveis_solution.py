3108fefd5f1b69c8c53cb2e1796e538ef87d0fc4
thaibbraz <thainabbraz@gmail.com>
2024-04-04 20:53:53 +0200

    adding examples.py and solutions

--- variaveis_solution.py
+++ variaveis_solution.py
@@ -9,8 +9,8 @@ in the console.
 
 After confirming that it works, comment it out.
 '''
-
-print("hello me")
+print("space project") #23pm
+#print("hello me")
 
 '''
 2)
@@ -18,9 +18,11 @@ Using both the (+) and the (*) operators,
 multiply and add any combination of numbers
 together so that you get the number 10.
 '''
-
-# print()
-
+a = 7
+b = 3
+result = a + b
+print(result)
+# print(
 '''
 3)
 print() is often used to debug
@@ -29,10 +31,11 @@ arguments inside of print() (which are
 separated by a comma). Add everything
 below into print()!
 '''
-
+print(3 + 4, "should equal 7")
 # print(3 + 4, " should equal 7")
 
 '''
+#TODO
 4)
 Add two strings together so that you
 are printing your full name!
@@ -49,17 +52,17 @@ to add a new line of code (do not delete any currently
 written code). Note: you will also have to un-comment
 the print to print the location.
 '''
-
+#TODO
 name = "Mia"
 current_location = "school"
-# print(name, " is currently at ", current_location)
+print(name, " is currently at ", current_location)
 
 '''
 6)
 Create variables so that the message is printed
 to the screen!
 '''
-
+#TODO
 # print(person, " went to the ", place, " to eat ", food, ".")
 
 '''
@@ -67,7 +70,7 @@ to the screen!
 Now, reassign all the previous variables so that
 a new message is printed to the screen!
 '''
-
+#TODO
 # print(person, " went to the ", place, " to eat ", food, ".")
 
 '''
@@ -78,12 +81,12 @@ However, there is a problem with how
 the variable has been declared. Debug it!
 '''
 
-# count = 0
-# count = count + 1
-# count += 1
-# count += 1
+count = 0
+count = count + 1
+count += 1
+count += 1
 
-# print(count, " should be 3")
+print(count, " should be 3")
 
 '''
 9)
@@ -100,40 +103,41 @@ you are unsure!
 a = 1
 b = 2
 
-# print("BEFORE > a is: ", a, " - and b is: ", b)
+print("BEFORE > a is: ", a, " - and b is: ", b)
 
-# temp = a
-# a = b
-# b = temp
+temp = a
+a = b
+b = temp
 
-# print("AFTER > a is: ", a, " - and b is: ", b)
+print("AFTER > a is: ", a, " - and b is: ", b)
 
 '''
 10)
 Make the statement below evaluate
 to true!
 '''
+#TODO
 
-# print(3 > 4)
+print(3 > 4)
 
 '''
 11)
 Make the statement below evaluate
 to false.
 '''
-
+#TODO
 x = 10
 y = x
 
-# print(x >= y)
+print(x >= y)
 
 '''
 12)
 Fix the comparison operator so that
 the statement evaluates to false.
 '''
-
-# print(3 == "3")
+#TODO
+print(3 == "3")
 
 '''
 13)
@@ -144,7 +148,7 @@ so that it does not evaluate to false.
 sunny = True
 warm = True
 
-# print(sunny != warm)
+print(sunny == warm)
 
 '''
 14)
@@ -157,3 +161,7 @@ you've learned so far.
     - Use the operators: (=) (+=) (-=) (*=)(++) (--).
     - Use the operators: (>) (<) (>=) (<=) (== vs ===) (!=).
 '''
+
+''' eu consegui fazer mas tenho duvidas, 1 achei que tinha 
+apagado e reiniciado, mas foi salvo, vou te perguntar sobre isso
+sobre o not nas booleans tambem vou te perguntar'''
