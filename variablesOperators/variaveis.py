d9c984a429c9e6e4e53e4adbd2611eb21b7ebf90
thaibbraz <thainabbraz@gmail.com>
2024-04-11 19:48:52 +0200

    exercicios categorizados e colocados em pastas

---
+++ variablesOperators/variaveis.py
@@ -0,0 +1,179 @@
+'''
+1)
+Using print() will print anything
+between the parentheses into your console.
+
+Change the phrase being printed below, 
+save, and then view your changes 
+in the console.
+
+After confirming that it works, comment it out.
+'''
+print("space project") #23pm
+#print("hello me")
+
+'''
+2)
+Using both the (+) and the (*) operators,
+multiply and add any combination of numbers
+together so that you get the number 10.
+'''
+a = 2
+b = 4
+c = 2
+result = 2 * 4 + 2
+print (result)
+# print()
+
+'''
+3)
+print() is often used to debug
+issues. You can actually add multiple
+arguments inside of print() (which are
+separated by a comma). Add everything
+below into print()!
+'''
+
+# print(3 + 4, " should equal 7")
+
+'''
+4)
+Add two strings together so that you
+are printing your full name!
+'''
+a = "luana "
+b = "bereza "
+result = a + b
+print (result)
+# print(result)
+print ("luana" + "bereza")
+
+'''
+5)
+Change Mia's location by reassigning the location
+variable to a new string! To do this, you will have 
+to add a new line of code (do not delete any currently
+written code). Note: you will also have to un-comment
+the print to print the location.
+'''
+
+name = "Mia"
+current_location = "school"
+# print(name, " is currently at ", current_location)
+
+'''
+6)
+Create variables so that the message is printed
+to the screen!
+'''
+
+# print(person, " went to the ", place, " to eat ", food, ".")
+
+'''
+7)
+Now, reassign all the previous variables so that
+a new message is printed to the screen!
+'''
+
+# print(person, " went to the ", place, " to eat ", food, ".")
+
+'''
+8)
+There are three ways to increase the
+number stored in the variable "count".
+However, there is a problem with how 
+the variable has been declared. Debug it!
+'''
+
+# count = 0
+# count = count + 1
+# count += 1
+# count += 1
+
+# print(count, " should be 3")
+
+'''
+9)
+Swap time! In the code below, we want to swap
+the values being stored in both "a" and "b".
+
+This code written below does not work. To accomplish
+this task, you will need to create an additional
+variable (you can name it "temp") to store either
+"a" or "b" while you are swapping. Draw it out if
+you are unsure!
+'''
+
+a = 1
+b = 2
+
+# print("BEFORE > a is: ", a, " - and b is: ", b)
+
+# temp = a
+# a = b
+# b = temp
+
+# print("AFTER > a is: ", a, " - and b is: ", b)
+
+'''
+10)
+Make the statement below evaluate
+to true!
+'''
+
+# print(3 > 4)
+
+'''
+11)
+Make the statement below evaluate
+to false.
+'''
+
+x = 10
+y = x
+
+# print(x >= y)
+
+'''
+12)
+Fix the comparison operator so that
+the statement evaluates to false.
+'''
+
+# print(3 == "3")
+
+'''
+13)
+Change something in the expression below
+so that it does not evaluate to false.
+'''
+
+sunny = True
+warm = True
+
+# print(sunny != warm)
+
+'''
+14)
+Practice with everything you've learned so far!
+
+Come up with 10 expressions that use what
+you've learned so far.
+    - Use the data types: string, int, float, boolean, None.
+- Use the following operators: (+) (-) (*) (/).
+    - Use the operators: (=) (+=) (-=) (*=)(++) (--).
+    - Use the operators: (>) (<) (>=) (<=) (== vs ===) (!=).
+'''
+''' eu consegui fazer mas tenho duvidas, 1 achei que tinha 
+apagado e reiniciado, mas foi salvo, vou te perguntar sobre isso
+sobre o not nas booleans tambem vou te perguntar'''
+
+auxiliar = None 
+y = 2 
+z = 5 
+auxiliar = y
+y = z
+z = auxiliar 
+print(z,y)
+
+
