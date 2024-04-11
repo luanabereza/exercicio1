064bb8dcfe3b1387be97a2fce05f477f57ef926d
thaibbraz <thainabbraz@gmail.com>
2024-04-11 20:52:44 +0200

    exemplos adicionados

---
+++ ifConditions/example.py
@@ -0,0 +1,61 @@
+'''
+Verificação de Maior Número:
+Peça ao usuário para inserir dois números e escreva um programa para verificar qual é o maior número e imprimir na tela.
+'''
+
+
+'''
+Classificação de Idade:
+Solicite ao usuário que insira sua idade e, em seguida, 
+escreva um programa para classificar a idade e<
+
+
+como criança, adolescente, adulto, etc.
+'''
+idade = int(input("Digite sua idade: "))
+
+if idade < 13:
+    print("Criança")
+elif idade < 20:
+    print("Adolescente")
+elif idade < 65:
+    print("Adulto")
+else:
+    print("Idoso")
+
+
+
+'''
+Verificação de Número Positivo ou Negativo:
+Peça ao usuário para inserir um número e escreva um programa para verificar se o número é positivo, negativo ou zero.
+'''
+
+'''
+Verificação de Número Primo:
+Peça ao usuário para inserir um número e escreva um programa para verificar se o número é primo ou não.
+'''
+'''
+Calculadora Simples:
+Escreva um programa que solicite ao usuário que insira dois números e uma operação (adição, subtração, multiplicação ou divisão) e, em seguida, execute a operação selecionada.
+'''
+'''
+Conversor de Unidades:
+Crie um programa que peça ao usuário para inserir uma medida em metros e ofereça opções para converter essa medida em centímetros, quilômetros ou milhas.
+'''
+'''
+Verificação de Triângulo:
+Solicite ao usuário que insira os comprimentos dos três lados de um triângulo e escreva um programa para verificar se é um triângulo válido (ou seja, se a soma de quaisquer dois lados é maior que o terceiro lado).
+'''
+'''
+Jogo de Adivinhação:
+Crie um programa que gere um número aleatório e peça ao usuário para adivinhá-lo. O programa deve fornecer dicas (maior, menor) até que o usuário adivinhe corretamente.
+'''
+
+'''
+Verificação de Vogal ou Consoante:
+Peça ao usuário para inserir uma letra e escreva um programa para verificar se é uma vogal ou uma consoante.
+'''
+'''
+Validação de Nome de Usuário:
+Peça ao usuário para inserir um nome de usuário e verifique se ele atende aos critérios de validade (por exemplo, deve ter pelo menos 6 caracteres e conter apenas letras e números).
+'''
