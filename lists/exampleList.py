d36fcdb778907aec75b2e0c5b28db85d7f85564c
luanabereza <luanabereza1991@outlook.com>
2024-04-15 01:01:25 +0200

    troquei os numeros por cores para testar com strings

---
+++ lists/exampleList.py
@@ -0,0 +1,21 @@
+# Creating a list of integers
+colors = ["azul", "amarelo", "vermelho", "verde", "laranja", ]
+print(colors[2])
+#ages = [10, 20, 30, 40, 50]
+#        0  1   2   3   4
+# Accessing the list element
+#print(ages[0])
+
+# Adding an element to the list
+colors.append("turquesa")
+print (colors)
+#print(ages)
+
+# Modifying an element
+#ages[0] = 100
+#print(ages)
+
+colors[1] = "dourado"
+
+print(colors)
+
